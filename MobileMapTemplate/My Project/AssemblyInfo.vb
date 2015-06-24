93ab6cdbfe9c35f781b8e4a7c09d04013abbde07
Michael Miller <mmiller@esri.com>
2015-06-23 20:30:01 -0400

    Final updates for release

--- MobileMapTemplate/My Project/AssemblyInfo.vb
+++ MobileMapTemplate/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("Infrastructure Mobile Map")> 
-<Assembly: AssemblyDescription("ArcGIS Mobile Map Template")> 
+<Assembly: AssemblyTitle("Solutions Windows Mobile Map")> 
+<Assembly: AssemblyDescription("Solutions Windows Mobile Map")> 
 <Assembly: AssemblyCompany("Esri")> 
-<Assembly: AssemblyProduct("Infrastructure Mobile Map")> 
-<Assembly: AssemblyCopyright("Copyright © Esri 2014")> 
+<Assembly: AssemblyProduct("Solutions Windows Mobile Map")> 
+<Assembly: AssemblyCopyright("Copyright © Esri 2015")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersionAttribute("10.2.0.0")> 
-<Assembly: AssemblyFileVersionAttribute("10.2.0.0")> 
+<Assembly: AssemblyVersionAttribute("10.3.1.0")> 
+<Assembly: AssemblyFileVersionAttribute("10.3.1.0")> 
